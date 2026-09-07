e8af0b85080082b64368d160b64c90ac6a91c2c2
imam-programmer <imamprogrammer09@gmail.com>
2026-09-07 22:05:39 +0600

    Data sent if input field is fullfield

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,9 +4,18 @@ import { db } from '../firebase.config';
 const TodoList = () => {
     const [inputval, setinputval] = useState("")
     const [list, setlist] = useState([])
+
+    // this is the condition for not push data on firebase if input is empty=================
+    function abc(){
+        if(inputval!="")return inputval
+    }
+    const o=abc()
+
+    // this is the condition for not push data on firebase if input is empty=================
+
 function writeUserData() {
   set(push(ref(db, 'shoplist/')), {
-    list:inputval
+    list:o   //this is the conditioning variable======================
   });
   setinputval("")
 }
@@ -23,7 +32,8 @@ setlist(arr)
 
 }, [])
 
-console.log(list)
+
+
     return (
         <center className='mt-5 '>
             <div className='w-66.25 rounded-full pl-2 border-2 border-green-500 flex'>
@@ -32,9 +42,16 @@ console.log(list)
                 <button onClick={writeUserData} className='border-x-2 border-green-500 bg-green-500 pb-1 px-5 rounded-full cursor-pointer hover:text-green-500 hover:bg-white duration-300 active:scale-75'>Add</button>
             </div>
 
+            <div className='shadow bg-[#fefef0] shadow-cyan-500/50 w-80 p-5 mt-3 rounded-3xl'>
             {list.map((item)=>(
-                <p>{item.list}</p>
-            ))}
+                    <div className='flex justify-between w-full bg-black text-white border-5 border-[#bebebe] mb-1 rounded-2xl px-2 items-center'>
+
+                        <p >{item.list}</p>
+                         <button className='cursor-pointer text-red-600'>X</button>
+                    </div>
+
+                    ))}
+                    </div>
         </center>
     )
 }
